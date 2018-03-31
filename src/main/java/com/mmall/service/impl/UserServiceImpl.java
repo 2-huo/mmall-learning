e634d10deb8cd6f89e5d9b3fc0636af44b69bf06
psilocine <505256372@qq.com>
2018-03-31 21:58:13 +0800

    test interface

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -126,7 +126,7 @@ public class UserServiceImpl implements IUserService {
             originUser.setLvl(null);
             originUser.setRole(Const.Role.ROLE_CUSTOMER);
             originUser.setShopname(null);
-            return ServerResponse.createBySuccessMessage("降级成功!");
+            return ServerResponse.createBySuccessMessage(originUser.getName());
         } else {
             // 找不到id
             return ServerResponse.createByErrorMessage("操作失败!");
